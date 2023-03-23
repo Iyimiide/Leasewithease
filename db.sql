d961a7928ea8785e046dc9d3ad0f249f666bfbc0
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-03-23 21:33:18 +0000

    Update and rename reglog.sql to db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 18, 2021 at 03:19 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +10,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `reglog`
+-- Database: `db`
 --
 
 -- --------------------------------------------------------
